70c7d1121702b819bd2527acc48f76e03ad96aea
Carlos Felipe Araújo <carlosxfelipe@gmail.com>
2022-11-14 13:43:39 -0300

    first commit

--- utils.js
+++ utils.js
@@ -44,6 +44,7 @@ const isValidCPF = (cpf) => {
   cpf = cpf.replace(/[^\d]+/g, "");
   if (cpf.length !== 11 || !!cpf.match(/(\d)\1{10}/)) return false;
   cpf = cpf.split("");
+
   const validator = cpf
     .filter((digit, index, array) => index >= array.length - 2 && digit)
     .map((el) => +el);
@@ -56,5 +57,6 @@ const isValidCPF = (cpf) => {
       10) %
       11) %
     10;
+
   return !(rest(10, 2) !== validator[0] || rest(11, 1) !== validator[1]);
 };
